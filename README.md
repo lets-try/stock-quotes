fbd59b43547648f0ae78edda760fca7ea5bd1163
Puja Gupta <pujag@IN-Pujag-2.local>
2019-04-30 00:30:19 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Simple webservice to learn project reactor
+
+The service provides realtime stock quotes.
+
+The stock prices changes as per a simple algorithm using random numbers.
---
+++ src/main/kotlin/com/learnreactor/stockquotes/StockQuotesApplication.kt
@@ -0,0 +1,11 @@
+package com.learnreactor.stockquotes
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StockQuotesApplication
+
+fun main(args: Array<String>) {
+	runApplication<StockQuotesApplication>(*args)
+}
